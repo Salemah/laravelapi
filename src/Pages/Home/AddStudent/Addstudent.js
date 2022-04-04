8ab65333832849df784f8a837cf23a191c770689
Salemah <kadertanvir1@gmail.com>
2022-04-05 00:53:21 +0600

    modal added

--- src/Pages/Home/AddStudent/Addstudent.js
+++ src/Pages/Home/AddStudent/Addstudent.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 
 const Addstudent = () => {
     const [student,setStudent] = useState('');
+    const [addsuccess,setAddsuccess] = useState(false);
     
     const handleOnChange = e => {
         const feild = e.target.name;
@@ -14,21 +15,18 @@ const Addstudent = () => {
 
 
     }
+    const close= ()=>{
+        setAddsuccess(false);
+    }
     const handleaddservice = e => {
         const students = {
             ...student
         }
         axios.post('http://localhost:8000/api/student/add', students)
-        // axios('http://localhost:8000/api/student/add', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(students)
-        // })
             .then(res => {
                 if(res.status=200){
-                    alert('success');
+                    setAddsuccess(true);
+                  
                 
                 }
             })
@@ -57,15 +55,33 @@ const Addstudent = () => {
                                 <input type="text" name='sid' className="form-control"  onBlur={handleOnChange}  />
                             </div>
 
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary"data-bs-toggle="modal" data-bs-target="#exampleModal" >Submit</button>
                         </form>
                     </div>
 
                 </div>
+                {/*  */}
+                {addsuccess &&
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                   
+                     <div class="modal-body">
+                      <p>New student added succfeull</p>
+                    </div> 
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"onClick={close} >Close</button>
+                      
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>}
 
             </div>
             
         </div>
+        
     );
 };
 
