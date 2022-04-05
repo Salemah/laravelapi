22df53779d8197a8f541f4a9d4833f78cbee4a41
Salemah <kadertanvir1@gmail.com>
2022-04-05 11:24:24 +0600

    edit modal added

--- src/Pages/Home/Student/Student.js
+++ src/Pages/Home/Student/Student.js
@@ -2,68 +2,87 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 const Student = () => {
-    const [student,setStudent] = useState([]);
-    useEffect(function(){
+    const [student, setStudent] = useState([]);
+    useEffect(function () {
         axios.get("http://localhost:8000/api/student/all")
-        .then(function(rsp){
-            setStudent(rsp.data);
-        },function(err){
+            .then(function (rsp) {
+                setStudent(rsp.data);
+            }, function (err) {
 
-        });
-    },[student]);
-    const handledelete =id =>{
+            });
+    }, [student]);
+    const handledelete = id => {
         const confirm = window.confirm("Are you sure to delete this students");
-        if (confirm) 
-        {
+        if (confirm) {
             axios.post(`http://localhost:8000/api/student/delete/${id}`)
-            .then(res => {
-                if(res.status=200){
-                    alert("delete succefull Successfully");
-                  
-                
-                }
-            })
-            // fetch(`http://localhost:8000/api/student/delete/${id}`, {
-            //   method: 'post',
-            //   headers: {
-            //     'content-type': 'application/json'
-            //   }
-            // })
-            //   .then(res => res.json())
-            //   .then(data => {
-            //     if (data.status=200) {
-            //       alert("delete succefull Successfully");
-            //     }
-            //   })
-            
-          }
+                .then(res => {
+                    if (res.status = 200) {
+                        alert("delete succefull Successfully");
+
+
+                    }
+                })
+        }
+
+    }
+    const handleEdit = id => {
+        // axios.post(`http://localhost:8000/api/student/delete/${id}`)
+        //     .then(res => {
+        //         if (res.status = 200) {
+        //             alert("delete succefull Successfully");
+
+
+        //         }
+        //     })
+
+
+
 
     }
 
     return (
         <div>
             <div className="container">
-  <div className="row">
-      {
-          student.map(st=>
-            <div className="col bg-primary text-white m-2 py-4 rounded"  key={st.id}>
-            
-           <p>{st.name}</p>
-           <p>{st.email}</p>
-           <p>{st.sid}</p>
-           <button onClick={()=>handledelete(st.id)}  >Delete</button>
-          </div>
-            )
-      }
-   
-    
-  </div>
-</div>
-           
+                <div className="row">
+                    {
+                        student.map(st =>
+                            <div className="col bg-primary text-white m-2 py-4 rounded" key={st.id}>
+
+                                <p>{st.name}</p>
+                                <p>{st.email}</p>
+                                <p>{st.sid}</p>
+                                <button onClick={() => handledelete(st.id)}  >Delete</button>
+                                <button data-bs-toggle="modal" href="#exampleModalToggle" onClick={() => handleEdit(st.id)}  >Edit</button>
+                            </div>
+                        )
+                    }
+
+
+                </div>
+
+                <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalToggleLabel">Modal 1</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Show a second modal and hide this one with the button below.
+                            </div>
+                            <div class="modal-footer">
+                                <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+
     );
 };
 
 export default Student;
 
- 
